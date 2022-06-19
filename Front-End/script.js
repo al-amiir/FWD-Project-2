f2825b0e891adadd5aff725007b0dc24a4181516
al-amiir <engineeralamiir@gmail.com>
2022-06-19 16:38:17 +0200

    Upload Edits

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,21 +1,25 @@
 const apiKey = "5b29d3a944cf570aed7ff89539a2bce5";
 async function fetchWithZipCode(zipCode) {
   try {
+    // Fetch Data From EndPoint
     let response = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${apiKey}`
     );
+
     let data = await response.json();
-    let serverResponse = await fetch("http://localhost:7000/weather", {
+    // Send Data To Server
+    let serverResponse = await fetch("http://localhost:7000/addWeather", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
+    // Recive Data From Server
     let serverData = await serverResponse.json();
     return serverData;
   } catch (error) {
-    console.log({ error });
+    return error.message;
   }
 }
 
@@ -24,27 +28,31 @@ document.querySelector("#generate").addEventListener("click", async (e) => {
   let dom = "";
   let zipCode = document.querySelector("#zip").value;
   let data = await fetchWithZipCode(zipCode);
-  data.list.map((d) => {
-    dom += `
-    <tr>
-        <td> ${d.main.temp} </td>
-        <td> ${d.main.feels_like} </td>
-        <td> ${d.dt_txt} </td>
-    </tr>
-
-
-`;
-  });
-
-  document.querySelector(
-    "#cityName"
-  ).textContent = ` City Name: ${data.city.name}`;
-  document.querySelector("#entryHolder_table").innerHTML = `
-    <tr>
-        <th>TEMP</th>
-        <th>FEEL</th>
-        <th>DATE</th>
-    </tr>
-    ${dom}
-`;
+  if (data.cod === "200") {
+    data.list.map((d) => {
+      dom += `
+              <tr>
+                  <td> ${d.main.temp} </td>
+                  <td> ${d.main.feels_like} </td>
+                  <td> ${d.dt_txt} </td>
+              </tr>
+            `;
+    });
+    document.querySelector(
+      "#cityName"
+    ).textContent = `City Name: ${data.city.name}`;
+    document.querySelector("#entryHolder_table").innerHTML = `
+      <tr>
+          <th>TEMP</th>
+          <th>FEEL</th>
+          <th>DATE</th>
+      </tr>
+      ${dom}
+  `;
+  } else {
+    document.querySelector("#cityName").textContent = "";
+    document.querySelector(
+      "#entryHolder_table"
+    ).innerHTML = `${data.message}, Please try another zip-code`;
+  }
 });
--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
+const port = 7000;
+let projectData = {};
+
 app.use(cors());
 app.use(bodyParser.json());
 
-let projectData = {};
-
-port = 7000;
 app.listen(port, () => {
   console.log(`Server Listening on Port ${port}`);
 });
 
-app.post("/weather", (req, res) => {
-  //   projectData[req.body.city.id] = req.body;
-  //   projectData = {
-  //     [req.body.city.id]: req.body,
-  //   };
+app.post("/addWeather", (req, res) => {
   projectData = req.body;
   res.send(JSON.stringify(projectData));
 });
